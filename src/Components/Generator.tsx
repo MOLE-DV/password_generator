5a58688a5438ef215fc78a4e1dd21b06d1f76cb1
MOLE-DV <maxx.olejnik@gmail.com>
2024-05-09 10:55:35 +0200

    password generation

--- src/Components/Generator.tsx
+++ src/Components/Generator.tsx
@@ -1,5 +1,5 @@
 import resetIcon from '../Icons/restart-svgrepo-com.svg'
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 
 
@@ -25,6 +25,7 @@ const Generator = () => {
     }
 
     const [settings, setSettings] = useState<settingsDict>({uppercase: true, lowercase: true, specialchars: true, numbers: true, length: 10});
+    const [sliderLen, setsliderLen] = useState(0);
 
     const checkboxHandler = (event: any) => {
         const checkbox = {name: event.target.name, value: event.target.checked};
@@ -37,6 +38,7 @@ const Generator = () => {
     }
 
     const sliderHandler = (event: any) => {
+        setsliderLen(Number(event.target.value))
         setSettings(
             {
                ...settings,
@@ -45,19 +47,8 @@ const Generator = () => {
         );
     }
 
-    const generate = async () => {
-        const filteredKeys: (keyof Symbols)[] = Object.keys(symbols).filter(key => settings[key])
+    const [ password, setPassword ] = useState("");
 
-        const usable_symbols:Partial<Symbols> = filteredKeys.reduce((acc, key) => {
-            acc[key] = symbols[key];
-            return acc;
-        }, {} as Partial<Symbols>);
-
-        console.log(symbols.length);
-        for(let i: number = settings.length as number; i > 0; i--) {
-            await generatorAnimation(usable_symbols);
-        }
-    }
 
     const getRandom = (max: number) => {
         return Math.floor(Math.random() * max);
@@ -71,18 +62,38 @@ const Generator = () => {
         return random_section[getRandom(random_section.length)]
     }
 
-    //FIXME: animationgen
-    const generatorAnimation = async (symbols: any) => {
-        const animation_time:number = 1;
+    const generate = async () => {
+        const filteredKeys: (keyof Symbols)[] = Object.keys(symbols).filter(key => settings[key])
+
+        const usable_symbols:Partial<Symbols> = filteredKeys.reduce((acc, key) => {
+            acc[key] = symbols[key];
+            return acc;
+        }, {} as Partial<Symbols>);
+
+        if(Object.keys(usable_symbols).length === 0) return;
+
+        let pass:string = ''
+
+        for(let i: number = settings.length as number; i > 0; i--) {
+            await generatorAnimation(usable_symbols, pass);
+            pass += getRandomChar(usable_symbols);
+            setPassword(pass);
+        }
+    }
+
+
+    const generatorAnimation = async (symbols: any, pass: string) => {
+        const animation_time:number = .1;
         const animation_increment:number = 0.01;
         let current_time:number = 0;
         
-        let base_value:string = "";
+        let base_value:string = pass;
 
+        let char = '';
         while(current_time <= animation_time){
             
-            const char = await getRandomChar(symbols);
-            input.value = base_value + char;
+            char = await getRandomChar(symbols);
+            setPassword(base_value + char)
 
             current_time += animation_increment;
             await Sleep(animation_increment);
@@ -94,8 +105,8 @@ const Generator = () => {
         <div id="generator">
             <div id="top">
                 <div id="input">
-                    <input type="text" id="password" placeholder='Password' value={}/>
-                    <img src={resetIcon} />
+                    <input type="text" id="password" placeholder='Password' value={password}/>
+                    <img src={resetIcon} onClick={() => setPassword('')}/>
                 </div>
             </div>
             <div id="bottom">
@@ -117,7 +128,7 @@ const Generator = () => {
                 </div> 
                 <div className="slider">
                     <input type="range" name='length' onChange={sliderHandler} min={1} max={25}/>
-                    <div id="title">Length</div>
+                    <div id="title">Length {sliderLen}</div>
                 </div> 
                 <div id="generateButton" onClick={generate}>Generate</div> 
             </div>
